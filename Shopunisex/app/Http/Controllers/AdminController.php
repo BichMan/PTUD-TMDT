a58aec3183ef1ac547598f432b9e8c96deb0bc74
BichMan <91049537+BichMan@users.noreply.github.com>
2021-10-10 16:58:58 +0700

    10/10/2021
    10/10/21

---
+++ Shopunisex/app/Http/Controllers/AdminController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+use Illuminate\Support\Facades\Session;
+use App\Http\Requests;
+use Illuminate\Support\Facades\Redirect;
+session_start();
+
+class AdminController extends Controller
+{
+    public function index()
+    {
+        return view('admin_login');
+    }
+    public function show_dashboard()
+    {
+        return view('admin.dashboard');
+    }
+    public function dashboard(Request $request){
+        $admin_email = $request->admin_email;
+        $admin_password = md5($request->admin_password); //mật khẩu mã hóa kiểu md5 zip
+
+        $result = DB::table('tbl_admin')->where('admin_email',$admin_email)->where('admin_password',$admin_password)->first(); //lấy giới hạn 1 user
+        if($result){
+            Session::put('admin_name',$result->admin_name);
+            Session::put('admin_id',$result->admin_id);
+            return Redirect::to('/dashboard');
+        }else{
+            Session::put('message','Mật khẩu hoặc tài khoản sai. Vui lòng nhập lại');
+            return Redirect::to('/admin');
+        }
+        
+        // echo '<pre>';
+        // print_r ($result);
+        // echo '</pre>';
+    }
+    public function logout(){
+        Session::put('admin_name',null);
+        Session::put('admin_id',null);
+        return Redirect::to('/admin');
+    }
+}
---
+++ Shopunisex/routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController; 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoryProduct;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//FrontEnd - hoạt động giao tiếp người dùng
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/trang-chu',[HomeController::class, 'index']);
+
+//BackEnd - hoạt động ở server
+Route::get('/admin',[AdminController::class, 'index']);
+Route::get('/dashboard',[AdminController::class, 'show_dashboard']);
+Route::get('/logout',[AdminController::class, 'logout']);
+Route::post('/admin-dashboard',[AdminController::class, 'dashboard']);
+
+//Category Product
+Route::get('/add-category-product',[CategoryProduct::class, 'add_category_product']);
+Route::get('/edit-category-product/{category_product_id}',[CategoryProduct::class, 'edit_category_product']);
+Route::get('/delete-category-product/{category_product_id}',[CategoryProduct::class, 'delete_category_product']);
+Route::get('/all-category-product',[CategoryProduct::class, 'all_category_product']);
+
+Route::get('/unactive-category-product/{category_product_id}',[CategoryProduct::class, 'unactive_category_product']);
+Route::get('/active-category-product/{category_product_id}',[CategoryProduct::class, 'active_category_product']);
+
+Route::post('/save-category-product',[CategoryProduct::class, 'save_category_product']);
+
+Route::post('/update-category-product/{category_product_id}',[CategoryProduct::class, 'update_category_product']);
+
+
